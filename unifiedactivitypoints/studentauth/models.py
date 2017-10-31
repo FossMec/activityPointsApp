698435ef9af7545b49f3ac01fb047716a99d9f83
kurianbenoy <kurian.pro@gmail.com>
2017-10-31 18:18:28 +0530

    Add templates and static folder , Initial setup of studentauth

--- unifiedactivitypoints/studentauth/models.py
+++ unifiedactivitypoints/studentauth/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# class Student(models.Model)
--- unifiedactivitypoints/unifiedactivitypoints/settings.py
+++ unifiedactivitypoints/unifiedactivitypoints/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'studentauth',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -45,7 +47,7 @@ ROOT_URLCONF = 'unifiedactivitypoints.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
