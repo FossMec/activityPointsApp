9cb4e10e0632192c1bbc4915816f98bccc344efd
kurianbenoy <kurian.pro@gmail.com>
2017-11-08 20:44:18 +0530

    Added becrypt module and logout -not properly configured

--- unifiedactivitypoints/studentauth/views.py
+++ unifiedactivitypoints/studentauth/views.py
@@ -43,31 +43,16 @@ class Login(View):
         return render(request,self.template_name,{"form":form})
 
 
-# def login(request):
-#     if request.method == "POST":
-#         form_contents = LoginForm(request.POST)
-#         if form_contents.is_valid():
-#             form = form_contents.save(commit=False)
-#             name = form.cleaned_data['registration_no']
-#             pwd = form.cleaned_data['password']
-#             try:
-#                 result = UserProfile.objects.get(registration_no=name,password=pwd)
-#                 if result:
-#                     return redirect("Success")
-#             except User.DoesNotExist:
-#                 form = LoginForm()
-#                 return render(request, "studentauth/login.html", {
-#                     "form": form,
-#                 })
-#     else:
-#         form = LoginForm()
-#         return render(request, "studentauth/login.html", {
-#             "form": form,
-#         })
-
 
 class Success(View):
     template_name = "studentauth/success.html"
 
     def get(self,request,*args,**kwargs):
         return render(request,self.template_name)
+
+def logout(request):
+    try:
+        del request.session['member_id']
+    except KeyError:
+        pass
+    return HttpResponse("You're logged out.")
